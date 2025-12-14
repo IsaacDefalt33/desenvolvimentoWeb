6f24ebeb8db3d19b977fef1777cb90abfc902b91
IsaacDefalt33 <isaacgyamada@gmail.com>
2025-12-14 19:17:39 -0300

    Add files via upload

---
+++ PTI.js
@@ -0,0 +1,5 @@
+// Alterar título da notícia
+document.querySelector('.post__title').textContent = 'Brasil Ganha Medalha de Ouro em Todos os Esportes nas Olimpíadas 2024';
+
+// Alterar imagem (exemplo com seletor genérico)
+document.querySelector('img').srcset = 'https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=400&h=300&fit=crop';
